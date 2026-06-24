bc5eca59bcbb2993696c53af1f0fdd9189bca9be
MASAHIRO UCHIDA <uchida.m@northeastern.edu>
2026-06-24 16:25:13 -0700

    Commit 6/24 4pm

---
+++ data.py
@@ -0,0 +1,37 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+
+def get_agnews_dataloader(model_name, batch_size, test=False):
+    """
+    Get DataLoader object of AG News dataset.
+    :param model_name: Name of the model. Used for tokenization.
+    :param batch_size: Batch size.
+    :param test: If true, then get test data. If not, get training data.
+    :return: DataLoader object of AG News dataset.
+    """
+    dataset = load_dataset("wangrongsheng/ag_news")
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    def _tokenize(sample):
+        """
+        Tokenize given AG News data.
+        :param sample: One sample of AG News dataset.
+        :return: Tokenized AG News data.
+        """
+        tokenized_sample = tokenizer(sample["text"], padding="max_length", truncation=True, max_length=128)
+
+        return tokenized_sample
+
+    tokenized_dataset = dataset.map(_tokenize, batched=True)
+    tokenized_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
+
+    if test:
+        split = "test"
+    else:
+        split = "train"
+
+    dataset_split = tokenized_dataset[split]
+    dataloader = DataLoader(dataset_split, batch_size=batch_size, shuffle=True)
+
+    return dataloader
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import torch
+from transformers import AutoTokenizer
+from datasets import load_dataset
+import datasets
+import huggingface_hub
+import transformers
+
+from data import get_agnews_dataloader
+
+
 # これはサンプルの Python スクリプトです。
 
 # Shift+F10 を押して実行するか、ご自身のコードに置き換えてください。
@@ -11,6 +21,16 @@ def print_hi(name):
 
 # ガター内の緑色のボタンを押すとスクリプトを実行します。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    model_name = "bert-base-uncased"
+    batch_size = 16
+    agnews_train_loader = get_agnews_dataloader(model_name, batch_size)
+    agnews_test_loader = get_agnews_dataloader(model_name, batch_size, test=True)
+
+    batch = next(iter(agnews_train_loader))
+
+    print(batch.keys())
+    print(batch["input_ids"].shape)
+    print(batch["attention_mask"].shape)
+    print(batch["label"].shape)
 
 # PyCharm のヘルプは https://www.jetbrains.com/help/pycharm/ を参照してください
